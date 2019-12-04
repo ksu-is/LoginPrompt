07766dd52ccb6c0050d039582361eb704d452ccc
Azntm <54510171+Azntm@users.noreply.github.com>
2019-12-03 21:39:24 -0500

    Delete main.py

--- LoginPrompt/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/index/")
-def index():
-return render_template("login.html")
-
-if __name__ == "__main__":
-app.run(host='0.0.0.0', port=4000)
